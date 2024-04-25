7bde6e05a7a19cc89b3b3ab66e147ffd4bba9857
Evgenia Taschilina <82048374+Zhenya8@users.noreply.github.com>
2024-04-26 00:37:28 +0300

    Update Задание 5.py

--- Задание 5.py
+++ Задание 5.py
@@ -2,12 +2,10 @@ from mpi4py import MPI
 import numpy as np
 import time
 
-
 def generate_random_system(size):
     A = np.random.rand(size, size)
     b = np.random.rand(size)
     return A, b
-
 
 def gaussian_elimination(A, b):
     n = len(b)
@@ -21,28 +19,23 @@ def gaussian_elimination(A, b):
         x[i] = (b[i] - np.dot(A[i, i+1:], x[i+1:])) / A[i, i]
     return x
 
-
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
-
 system_sizes = [10, 100, 1000]
 
 for system_size in system_sizes:
     A, b = generate_random_system(system_size)
-
     chunk_size = system_size // size
     local_A = np.zeros((chunk_size, system_size))
     comm.Scatter(A, local_A, root=0)
     local_b = np.zeros(chunk_size)
     comm.Scatter(b, local_b, root=0)
-
     start_time = time.time()
     solution = gaussian_elimination(local_A, local_b)
     global_solution = np.zeros(system_size)
     comm.Gather(solution, global_solution, root=0)
     end_time = time.time()
-
     if rank == 0:
         print(f"System size: {system_size}, Time taken: {
             end_time - start_time} seconds")
